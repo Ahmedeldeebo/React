6fa6f452af790a5c2a97f33874de968298c4b759
Ahmed eldeeb <a_eldeebo@yahoo.com>
2022-02-25 11:49:52 +0200

    End of this Tut

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,12 +5,24 @@ class Counter extends Component {
     count: 0,
   };
 
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-      </React.Fragment>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
   }
   getBadgeClasses() {
